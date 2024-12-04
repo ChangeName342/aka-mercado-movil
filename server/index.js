d7d956b5e9687774ddc27e470634b2e3ccc31a31
ChangeName342 <ignaciobarrientosc5@gmail.com>
2024-12-04 14:44:32 -0300

    server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // IMPORTACIONES POR PAQUETES
 const express = require("express");
 const mongoose = require("mongoose");
-
+require("dotenv").config(); // Solo necesario si trabajas localmente con un archivo .env
 
 // IMPORTACIONES DE OTROS ARCHIVOS
 const authRouter = require("./routes/auth");
@@ -11,10 +11,10 @@ const userRouter = require("./routes/user");
 
 // INICIALIZACIÓN
 const PORT = process.env.PORT || 3000;
+const DB = process.env.MONGO_URI; // Obtenemos el URI desde las variables de entorno
+
 const app = express();
-const DB = 
-  "mongodb+srv://ignacio123:tuki1234@cluster0.gb0xq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
- 
+
 // middleware
 app.use(express.json());
 app.use(authRouter);
@@ -24,14 +24,14 @@ app.use(userRouter);
 
 // conexiones
 mongoose
-  .connect(DB)
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Conexión Exitosa");
+    console.log("Conexión Exitosa a MongoDB");
   })
   .catch((e) => {
-    console.log(e);
+    console.log("Error en la conexión:", e);
   });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Conexión al puerto " + PORT);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
